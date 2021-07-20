707abd2b2242742604a63fc155dd62f90735d717
Jean <jeanpunk01@gmail.com>
2021-07-19 21:58:59 -0300

    Creacion de metodos de Productos

--- Negocio/Producto.cs
+++ Negocio/Producto.cs
@@ -11,23 +11,97 @@ namespace Negocio
 
         public int? Id_Producto { get; set; }
 
-        [Required(ErrorMessage = "Ingrese el Nombre del producto"), MaxLength(30)]
-        [StringLength(100)]
-        [Display(Name = "Producto")]
         public string Nombre_producto { get; set; }
 
-        [Required(ErrorMessage = "Ingrese el precio del producto")]
-        [Display(Name = "Precio")]
-        [DataType(DataType.Currency)]
+      
         public double Precio_producto { get; set; }
 
-        [Required(ErrorMessage = "Ingrese la descripcion del producto")]
-        [StringLength(100)]
-        [Display(Name = "Descripcion")]
+       
         public string Descripcion { get; set; }
 
         public string Estado { get; set; }
 
 
+
+        #region Metodo Publico
+        public static List<Producto> Listar()
+        {
+            List<Producto> ListaProductos = new List<Producto>();
+
+            DataTable dt = Datos.Productos.Listar();
+
+            foreach (DataRow item in dt.Rows)
+            {
+
+                ListaProductos.Add(ArmadoDeDatos(item));
+
+            }
+
+            return ListaProductos;
+        }
+
+
+
+
+
+        public void Grabar()
+        {
+            if (Validar(out string error))
+            {
+                if (Id_Producto != null)
+                {
+                    Modificar();
+                }
+                else
+                {
+                    Agregar();
+                }
+
+
+            }
+            else
+                throw new Exception(error);
+
+
+
+        }
+
+        #endregion
+
+
+        #region Metodo Privados
+
+        public static void Eliminar(int id)
+        {
+            Datos.Productos.Eliminar(id);
+        }
+
+
+
+        private static Producto ArmadoDeDatos(DataRow dr)
+        {
+            Producto Producto = new Producto();
+
+            Producto.Id_Producto = Convert.ToInt32(dr["Id_Producto"]);
+            Producto.Nombre_producto = dr["Nombre_producto"].ToString();
+            Producto.Precio_producto = Convert.ToDouble(dr["Precio_producto"]);
+            Producto.Descripcion = dr["Descripcion"].ToString();
+            Producto.Estado = dr["Estado"].ToString();
+
+            return Producto;
+        }
+
+        private void Modificar()
+        {          
+
+            Datos.Productos.Modificar(Id_Producto, Nombre_producto, Precio_producto, Descripcion, Estado);
+        }
+
+        private int Agregar()
+        {
+
+            return Datos.Productos.Agregar(Nombre_producto, Precio_producto, Descripcion, Estado);
+        }
+        #endregion
     }
 }
