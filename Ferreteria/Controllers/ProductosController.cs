598ffe8f6b9b6146941314a18a5c96d69b0e8b58
Jean <jeanpunk01@gmail.com>
2021-07-19 21:52:38 -0300

    Clase Producto creada en Negocio

---
+++ Ferreteria/Controllers/ProductosController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ferreteria.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            List<Producto> ListaProductos = Producto.Listar();
+
+            return View(ListaProductos);
+        }
+
+        [HttpGet]
+        public ActionResult Edicion(int? id)
+        {
+           
+            try
+            {
+
+                if (id == null)
+                {
+                    return View(new Producto());
+                }
+                else
+                {
+                    Producto usuario = Producto.Listar().Where(x => x.Id_Producto == id).FirstOrDefault();
+                    return View(usuario);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Producto());
+        }
+
+        [HttpPost]
+        public JsonResult Guardar(Producto producto)
+        {
+            try
+
+            {
+                producto.Grabar();
+                return Json(new Resultado());
+            }
+            catch (Exception ex)
+            {
+                return Json(new Resultado(ex.Message, ex.HResult));
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Eliminar(int id)
+        {
+            try
+            {
+                Producto.Eliminar(id);
+                return Json(new Resultado());
+            }
+            catch (Exception ex)
+            {
+                return Json(new Resultado(ex.Message, ex.HResult));
+            }
+        }
+
+    }
+}
---
+++ Negocio/Producto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    class Producto
+    {
+
+        public int? Id_Producto { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el Nombre del producto"), MaxLength(30)]
+        [StringLength(100)]
+        [Display(Name = "Producto")]
+        public string Nombre_producto { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el precio del producto")]
+        [Display(Name = "Precio")]
+        [DataType(DataType.Currency)]
+        public double Precio_producto { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la descripcion del producto")]
+        [StringLength(100)]
+        [Display(Name = "Descripcion")]
+        public string Descripcion { get; set; }
+
+        public string Estado { get; set; }
+
+
+    }
+}
